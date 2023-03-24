f1b0a032dd75196bf909087dde4c5772fa16f9b4
QIU <1837703124@qq.com>
2023-03-25 02:11:41 +0800

    push commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,5 +7,7 @@ public class Main {
         System.out.println("Hello git3!");
         System.out.println("Hello git4!");
         System.out.println("hot-fix test");
+        System.out.println("push test!");
+
     }
 }
