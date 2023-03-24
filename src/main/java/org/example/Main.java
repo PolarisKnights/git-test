8eb1a2088227b37cc5e614b6abfeaa19bf5fab63
PolarisKnights <128772204+PolarisKnights@users.noreply.github.com>
2023-03-25 02:17:07 +0800

    pull commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,7 @@ public class Main {
         System.out.println("Hello git4!");
         System.out.println("hot-fix test");
         System.out.println("push test!");
+        System.out.println("poll test!");
 
     }
 }
